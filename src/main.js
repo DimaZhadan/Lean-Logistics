4dfe18bc7cd2603140b04f1d42fd4c2f63332fa8
Dima Zhadan <dimka.jadanxaxa@gmail.com>
2025-02-15 02:20:00 +0200

    fonts

--- src/main.js
+++ src/main.js
@@ -2,14 +2,24 @@ let btn = document.querySelector('.header-burger__btn');
 let btnClose = document.querySelector('.header-burger__btnclose');
 let menu = document.querySelector('.header-burger');
 let body = document.querySelector('body');
+let burgerBtn = document.querySelectorAll('.header-burger__element');
+
 btn.addEventListener('click', () => {
    menu.classList.toggle('active');
    body.classList.add('.noscroll');
-   // removeEventListener();
+   removeEventListener('click', btn);
 });
 btnClose.addEventListener('click', () => {
    menu.classList.remove('active');
    body.classList.remove('.noscroll');
-   // removeEventListener();
+   removeEventListener('click', btnClose);
 });
+burgerBtn.forEach((elem) => {
+   elem.addEventListener('click', () => {
+      menu.classList.remove('active');
+      body.classList.remove('.noscroll');
+      removeEventListener('click', elem);
+   })
+})
+
 
